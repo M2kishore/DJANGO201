02c6e3a1c515802f4dc0f47e4f2fe11c4c9874bd
M M Kishore <kishoremeenakshi2001@gmail.com>
2022-01-21 08:51:47 +0530

    Dynamic routing

--- server.py
+++ server.py
@@ -7,12 +7,15 @@ class MyServer(SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header("content-type", "text/html")
         self.end_headers()
-
-        content = f"<h1>{datetime.now()}<h1>"
+        print(self.path)# this is the current path
+        if self.path == "/hello":
+            content = content = f"<h1>{datetime.now()}<h1>"
+        else:
+            content = "<h1>Error</h1>"
         self.wfile.write(content.encode())
 
 address = "127.0.0.1"
-port = 8000
+port = 3000
 server_address = (address,port)
 httpd = HTTPServer(server_address, MyServer)
 httpd.serve_forever()
