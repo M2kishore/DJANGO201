3c5356bb3d3adbf47d6e9aa0661b8a737a649ee3
M M Kishore <cb.en.u4cse19127@cb.students.amrita.edu>
2022-01-27 22:16:21 +0530

    moved views to views.py and performed migrations included ORM

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from tasks.models import Task
+
+
+def task_view(request):
+    tasks = Task.objects.all()
+    search_string = request.GET.get("search")
+    if search_string:
+        tasks = tasks.filter(title__icontains=search_string)
+    return render(request, "tasks.html", {"tasks": tasks})
+
+
+def add_task_view(request):
+    task_string = request.GET.get("task")
+    Task(title=task_string).save()
+    return HttpResponseRedirect("/tasks/")
+
+
+def delete_task_view(request, id):
+    Task.objects.filter(id=id).delete()
+    return HttpResponseRedirect("/tasks/")
