d67a6f51cdd60ae52a66e9b6a5d7e3ef05dc3fa1
M M Kishore <cb.en.u4cse19127@cb.students.amrita.edu>
2022-02-02 12:12:45 +0530

    registered tasks to admin added users as foreign ket to each tasks

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from tasks.models import Task
+
+admin.sites.site.register(Task)
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # tasks/models.py
@@ -7,6 +8,7 @@ class Task(models.Model):
     description = models.TextField()
     completed = models.BooleanField(default=False)
     deleted = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.title
