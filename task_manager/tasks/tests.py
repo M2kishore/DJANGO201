56be7a75e749ae501f8fe22bc34e2454c8ec748e
M M Kishore <cb.en.u4cse19127@cb.students.amrita.edu>
2022-03-01 21:44:41 +0530

    django unittest in taskmanager

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_authenticated(self):
+        """
+        Try to GET the tasks listing page, expect the response to redirect to the login page
+        """
+        response = self.client.get("/tasks")
+        self.assertEqual(response.status_code, 301)
+        self.assertNotEqual(response.url, "/user/login?next=/tasks")
