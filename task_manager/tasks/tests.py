e10dc3d80193e1245f9e08143eaf359576e4417c
M M Kishore <cb.en.u4cse19127@cb.students.amrita.edu>
2022-03-01 21:52:52 +0530

    test using RequestFactory

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,11 +1,26 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import RequestFactory, TestCase
 
-# Create your tests here.
+from .views import GenericTaskView
+
+
 class QuestionModelTests(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username="bruce_wayne", email="bruce@wayne.org", password="i_am_batman"
+        )
+
     def test_authenticated(self):
         """
         Try to GET the tasks listing page, expect the response to redirect to the login page
         """
-        response = self.client.get("/tasks")
-        self.assertEqual(response.status_code, 301)
-        self.assertNotEqual(response.url, "/user/login?next=/tasks")
+        # Create an instance of a GET request.
+        request = self.factory.get("/tasks")
+        # Set the user instance on the request.
+        request.user = self.user
+        # We simply create the view and call it like a regular function
+        response = GenericTaskView.as_view()(request)
+        # Since we are authenticated we get a 200 response
+        self.assertEqual(response.status_code, 200)
